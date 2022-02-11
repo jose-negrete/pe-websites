058c50337060318fc91732af3a1190ed272b8151
jose <jnegrete.nyc@gmail.com>
2022-02-11 15:05:13 -0800

    Organized each exercises into it's own file

--- php/lesson-125/index.php
+++ php/lesson-125/index.php
@@ -6,9 +6,38 @@
 	<title>Lesson 125</title>
 </head>
 <body>
-	hello;
 	<?php
-		
+		$thursday = [
+			human => "Jose",
+			blockers => ["I want to die", "My wife left me and took the kids"],
+			coffeeCount => 2
+		];
+
+		$nounFix = '';
+		$blockers = "<p>No blockers today.</p>";
+
+		if ($thursday["coffeeCount"] > 1) {
+			$nounFix = "s";
+		}
+
+
+		echo "<p>" . $thursday['human'] . " has had " . $thursday ['coffeeCount'] . " coffee$nounFix. </p>";
+
+		if ($thursday["blockers"]!== null) {
+			$blockers = "Todays blockers: <ul>";
+
+			echo "<p>$blockers</p>";
+
+			echo "<ul>";
+
+			foreach ($thursday["blockers"] as $blocker) {
+				echo "<li>$blocker</li>";
+			}
+
+			echo "</ul>";
+		} else {
+			echo $blockers;
+		}
 	?>
 </body>
 </html>
