b00ef8b68ccafe01dacff89b55435f6908f755fa
jose <jnegrete.nyc@gmail.com>
2022-03-15 14:01:36 -0700

    Updated for more users

--- lesson-189/index.php
+++ lesson-189/index.php
@@ -10,47 +10,88 @@
 	<div class="inner-column">
 		<?php 
 			$userInfo = [
-				"username" => "kenny",
-				"password" => "login"
+				[
+					"username" => "kenny",
+					"password" => "login"
+				],
+				[
+					"username" => "Derek",
+					"password" => "Wood"
+				],
+				[
+					"username" => "Alex",
+					"password" => "Vong"
+				],
+				[
+					"username" => "Brian",
+					"password" => "Lowerey"
+				],
+				[
+					"username" => "Ivy",
+					"password" => "Reynolds"
+				],
+				[
+					"username" => "Shayne",
+					"password" => "McGregor"
+				],
+				[
+					"username" => "Joshua",
+					"password" => "Gage"
+				],
+				[
+					"username" => "Tara",
+					"password" => "Haskamp"
+				],
+				[
+					"username" => "Jevohn",
+					"password" => "Newsome"
+				]
 			];
 
-			if ( isset($_POST["submitted"]) ) {
+			$title = "Please Enter Your Username and Password";
 
-				$secretUsername = $userInfo["username"];
-				$secretPassword = $userInfo["password"];
+			if ( isset($_POST["submitted"]) ) {
 
 				$submittedUsername = $_POST["username"];
 				$submittedPassword = $_POST["password"];
 
 				$display = "";
 
-				if ( $secretUsername !== $submittedUsername || $secretPassword !== $submittedPassword ) { ?>
+				for ($i = 0; $i <= count($userInfo)-1; $i++) {
 
-					<h1>Wrong username and/or password</h1>
-					<h2>Hint: Hit song from Top Gun</h2>
+					$secretUsername = $userInfo[$i]["username"];
+					$secretPassword = $userInfo[$i]["password"];
+					
+					if ( $secretUsername === $submittedUsername || $secretPassword === $submittedPassword ) {
 
-				<?php }
+						$name = $secretUsername;
 
-				if ( $secretUsername === $submittedUsername && $secretPassword === $submittedPassword ) { ?>
+						$title = "Welcome to the Danger Zone $name";
 
-					<?php $display = "none"; ?>
+						$display = "none"; ?>
+						
+							<header>
+								<h1><?=$title?></h1>
+								<a href="./">Log out</a>
+							</header>	
 
-					<header>
-						<h1>Welcome to the danger zone.</h1>
-						<a href="./">Log out</a>
-					</header>	
+							<picture>
+								<iframe width="560" height="315" src="https://www.youtube.com/embed/kUsFWO08CO0?&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</picture>
 
-					<picture>
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/kUsFWO08CO0?&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					</picture>
+						<?php 
+							break;
+					}
 
-				<?php } 
-			} else { ?>
+					if ( $i >= count($userInfo)-1 ) {
+						$title = "Wrong Username and/or Password";
+					}
 
-				<h1>Please enter username and password...</h1>
-
-			<?php }
+				}
+			}
 		?>
+
+		<h1 style="display: <?=$display?>;"><?=$title?></h1>
 
 		<form method="POST" style="display: <?=$display?>;">
 			<div class="field">
@@ -61,12 +102,13 @@
 
 			<div class="field">
 				<label for="password">Password:</label>
-				
+		
 				<input type="password" id="password" name="password" required>
 			</div>
 
 			<button type="submit" name="submitted">Log in</button>
 		</form>
+
 	</div>
 </body>
 </html>
